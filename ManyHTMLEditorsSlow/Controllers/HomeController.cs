18292f6c923a6fef356d36666e29aaadb1b7397a
Rich Linnell <richlinnell@gmail.com>
2013-04-02 17:16:45 +0100

    Initial checkin

---
+++ ManyHTMLEditorsSlow/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ManyHTMLEditorsSlow.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!";
+
+            return View();
+        }
+    }
+}
